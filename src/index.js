ed7d2cedeb1f9753a8c3e22d264a0b5f9c71689c
himanigulati01 <himanigulati2001@gmail.com>
2021-08-31 12:15:45 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./db/mongoose");
 const userRoutes = require("./routers/user");
 const taskRoutes = require("./routers/task");
+const bcrypt = require("bcrypt")
 
 const app = express();
 
@@ -9,7 +10,19 @@ const port = process.env.PORT || 3000;
 const log = console.log;
 
 app.use(express.json()); //to get the request data in json format
+
+// ! Routes for user and task
 app.use(userRoutes);
 app.use(taskRoutes);
 
 app.listen(port, () => log("Server is up on port " + port));
+
+const myfunc =  async () => {
+    const PASSWORD = "himani@2001."
+    const HASHED_PASS = await bcrypt.hash(PASSWORD,8)
+    const isMatch = await bcrypt.compare("himani@2001.",HASHED_PASS)
+    console.log(HASHED_PASS)
+    console.log(isMatch)
+}
+
+myfunc()
